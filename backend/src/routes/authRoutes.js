68a5f9d218930b680119ba62015c7761080a4462
caetanaldo <gustavocaetanodalponte@gmail.com>
2026-04-28 10:55:04 -0300

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import authController from "../controllers/authController.js";
-import { validate } from "../middlewares/validate.middleware.js";
-import { registerSchema, loginSchema } from "../validators/authvalidator.js";
 
 const router = Router();
 
-router.post("/register", validate(registerSchema), authController.register);
-router.post("/login", validate(loginSchema), authController.login);
+router.post("/register", authController.register);
+router.post("/login", authController.login);
 
 export default router;
