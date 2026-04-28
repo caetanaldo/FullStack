bb64b7dd24d6ee88761c1e52a20510877bc18b82
ErickLSSousa <erickrewardd@gmail.com>
2026-04-28 10:46:34 -0300

    proteção do register para somente admin registrar

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import authController from "../controllers/authController.js";
 import { validate } from "../middlewares/validate.middleware.js";
 import { registerSchema, loginSchema } from "../validators/authvalidator.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import roleMiddleware from "../middlewares/roleMiddleware.js";
 
 const router = Router();
 
-router.post("/register", validate(registerSchema), authController.register);
+router.post("/register", authMiddleware, roleMiddleware("admin"), validate(registerSchema), authController.register);
 router.post("/login", validate(loginSchema), authController.login);
 
 export default router;
