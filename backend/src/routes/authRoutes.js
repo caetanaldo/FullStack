0d17a098f24b8565d3b6c5faf13ebc83ac6a210b
ErickLSSousa <erickrewardd@gmail.com>
2026-04-28 10:17:12 -0300

    validates

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authController from "../controllers/authController.js";
+import { validate } from "../middlewares/validate.middleware.js";
+import { registerSchema, loginSchema } from "../validators/authvalidator.js";
 
 const router = Router();
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.post("/register", validate(registerSchema), authController.register);
+router.post("/login", validate(loginSchema), authController.login);
 
 export default router;
